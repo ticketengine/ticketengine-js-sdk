427ffddfc2a7bdcbd32036a56f15d7598ab05bc7
Tom Timmerman <info@tomtimmerman.nl>
2019-11-11 15:05:12 +0100

    added customer email

--- src/command/customer.ts
+++ src/command/customer.ts
@@ -10,6 +10,7 @@ export interface CreateCustomerArguments extends CommandData {
     firstName?: string;
     lastName?: string;
     birthDate?: string;
+    email?: string;
     gender?: string;
     tags?: Array<string>;
 }
@@ -20,6 +21,7 @@ export interface ChangeCustomerArguments extends CommandData {
     firstName?: string;
     lastName?: string;
     birthDate?: string;
+    email?: string;
     gender?: string;
     tags?: Array<string>;
 }
