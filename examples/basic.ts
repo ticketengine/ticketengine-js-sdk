45e5b5f9b4ead968b6c75ce8fff9e35e69a2854a
Tom Timmerman <info@tomtimmerman.nl>
2020-04-15 10:14:47 +0200

    fixed bug

--- examples/basic.ts
+++ examples/basic.ts
@@ -22,16 +22,16 @@ async function run() {
     try {
         // const r = await client.tag.createTag({name: 'myTag'});
         // const r = await client.access.renameEvent({});
-        const r = await client.user.getAuthToken({
-            grantType: 'password',
-            clientId: 'ticket_engine_back_office',
-            clientSecret: 'fn4ZKSGyv34cwz5DYNXcUqheX63NRWgn',
-            scope: 'event:write customer:write user:write order:write event_manager:write tag:write sales_chanel:write payment:write',
-            username: 'admin',
-            password: 'AdfRvNNYZZbWy2xuU6KWnaNWunELEPh297r4mxWjvQ'
-        });
+        // const r = await client.user.getAuthToken({
+        //     grantType: 'password',
+        //     clientId: 'ticket_engine_back_office',
+        //     clientSecret: 'fn4ZKSGyv34cwz5DYNXcUqheX63NRWgn',
+        //     scope: 'event:write customer:write user:write order:write event_manager:write tag:write sales_chanel:write payment:write',
+        //     username: 'admin',
+        //     password: 'AdfRvNNYZZbWy2xuU6KWnaNWunELEPh297r4mxWjvQ'
+        // });
         // const r = await client.user.createUser({username: 'test', password: 'test', scopes: ['customer:write', 'event_manager:write']});
-        // const r = await client.tokens.createCoupon({name: 'test', description: 'lorum ipsum', useLimit: 1});
+        const r = await client.tokens.createCoupon({name: 'test', description: 'lorum ipsum', useLimit: 1});
 console.log(r);
     } catch (e) {
 console.error(e);
