eae92c89a45862bc0f862c85515136c148c1074d
Tom Timmerman <info@tomtimmerman.nl>
2020-06-16 11:39:46 +0200

    fixed optional field

--- src/command/order.ts
+++ src/command/order.ts
@@ -46,7 +46,7 @@ export interface CancelOrderArguments extends CommandData {
 }
 export interface CheckoutOrderArguments extends CommandData {
     aggregateId: string;
-    customerEmail: string;
+    customerEmail?: string;
 }
 export interface CompleteOrderArguments extends CommandData {
     aggregateId: string;
